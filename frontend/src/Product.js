d3a106c192ccfa3955bfc36f2e969380ec3125a5
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2024-02-27 01:13:47 +0700

    Edit Product.js
    -Make it better looks

--- frontend/src/Product.js
+++ frontend/src/Product.js
@@ -29,9 +29,9 @@ export default function Product() {
                 ID : {p._id}
                 <span> {p.name}</span>
                 <img src={p.img}/> 
-                {p.price}
-                <button onClick={() => onDeleteProducts(p._id)}>Delete</button>
-                <button onClick={() => onOkClick(p._id)}>ok</button>
+                {p.price}฿ 
+                <button onClick={() => onDeleteProducts(p._id)} style = {{margin: 5}}>Delete</button>
+                <button onClick={() => onOkClick(p._id)}>OK</button>
             </li>
         ));
     } else {
@@ -87,50 +87,63 @@ export default function Product() {
     if (!product) return null;
 
     return (
-        <div style = {{ textAlign: "center" }}>
-            <div style = {{ margin: "20px auto", maxWidth: "600px" }}>
+        <div style={{ textAlign: "center" }}>
+            <h1>WELCOME TO PRODUCTS MANAGEMENT WEB-APP</h1>
+            <div style={{ margin: "20px auto", maxWidth: "600px" }}>
+            <div>{message && <p>{message}</p>}</div>
+
+            <div style={{ display: "grid", gap: "10px"}}>
+                
                 <h1>Add Product</h1>
-                <div>{message && <p>{message}</p>}</div>
-                    <div>
-                        <label htmlFor = "product_id">ID:</label>
-                        <input type = "number" id = "product_id" ref = {product1} />
-                    </div>
-                    <div>
-                        <label htmlFor = "product_name">Product Name:</label>
-                        <input type = "text" id = "product_name" ref = {product2} />
-                    </div>
-                    <div>
-                        <label htmlFor = "product_price">Price:</label>
-                        <input type = "text" id="product_price" ref = {product3} />
-                    </div>
-                    <div>
-                        <label htmlFor = "product_img">Image URL:</label>
-                        <input type = "text" id = "product_img" ref = {product4} />
-                    </div>
-                <button onClick={onAddProduct}>Add Product</button>
+                <label htmlFor="product_id" style={{ textAlign: "left"}}>
+                ID:
+                </label>
+                <input type="number" id="product_id" ref={product1} />
+    
+                <label htmlFor="product_name" style={{ textAlign: "left"}}>
+                Product Name:
+                </label>
+                <input type="text" id="product_name" ref={product2} />
+    
+                <label htmlFor="product_price" style={{ textAlign: "left" }}>
+                Price:
+                </label>
+                <input type="text" id="product_price" ref={product3} />
+    
+                <label htmlFor="product_img" style={{ textAlign: "left" }}>
+                Image URL:
+                </label>
+                <input type="text" id="product_img" ref={product4} />
             </div>
-
-            <div style = {{ margin: "20px auto", maxWidth: "600px" }}>
-                <h1>Update Product</h1>
-                <h2>Select a product to update</h2>
-                <div>
-                    <label htmlFor="update_product_name">Product Name:</label>
-                    <input type = "text" id="update_product_name" ref = {update_product1} />
-                </div>
-                <div>
-                    <label htmlFor="update_product_price">Price:</label>
-                    <input type = "text" id="update_product_price" ref = {update_product2} />
-                </div>
-                <div>
-                    <label htmlFor = "update_product_img">Image URL:</label>
-                    <input type = "text" id = "update_product_img" ref = {update_product3} />
-                </div>
+            <button onClick = {onAddProduct} style={{fontSize: 25, margin: "20px auto"}}>Add Product</button>
             </div>
-
-            <div style={{ margin: "20px auto", maxWidth: "800px" }}>
+    
+            <div style={{ margin: "20px auto", maxWidth: "600px" }}>
+            <h1>Update Product</h1>
+            <h2>Select a product to update</h2>
+            <h2>Click [OK] for edit and click again to confirm</h2>
+            <div style={{ display: "grid", gap: "10px" }}>
+                <label htmlFor="update_product_name" style={{ textAlign: "left" }}>
+                Product Name:
+                </label>
+                <input type="text" id="update_product_name" ref={update_product1} />
+    
+                <label htmlFor="update_product_price" style={{ textAlign: "left" }}>
+                Price:
+                </label>
+                <input type="text" id="update_product_price" ref={update_product2} />
+    
+                <label htmlFor="update_product_img" style={{ textAlign: "left" }}>
+                Image URL:
+                </label>
+                <input type="text" id="update_product_img" ref={update_product3} />
+            </div>
+            </div>
+    
+            <div style={{ margin: "50px auto", maxWidth: "800px" }}>
                 <h1>Products</h1>
                 <ul style={{ listStyleType: "none", padding: 0 }}>{productList}</ul>
             </div>
-        </div>
+      </div>
     );
 }
