e68897cb6fe3ecc699ffce706fad3a2c79d860bb
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2024-02-26 23:21:58 +0700

    Install axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Product from './Product'
 import reportWebVitals from './reportWebVitals';
 
